6d3e8c7fb51726f4e346ede35928d107a2ba43ea
Andrew Wall <drewsmug@gmail.com>
2016-05-16 16:58:26 -0700

    Stubbing out the lexical analyzer file structure

---
+++ lex/lex.h
@@ -0,0 +1,13 @@
+/********************************************************************
+ * Input: A list of C files
+ *
+ * Output: A file called lex.out with the following format.
+ *         <token>:<token type>
+ *
+ *         token types: keyword        (eg: int, while)
+ *                      identifier     (eg: main)
+ *                      constant       (eg: 10,20)
+ *                      string         (eg: "hello")
+ *                      special symbol (eg: (,),{,})
+ *                      operator       (eg: +,-,*,/)
+ *******************************************************************/
